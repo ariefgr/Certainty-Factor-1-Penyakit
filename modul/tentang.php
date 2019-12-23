cb93779fd7008eca7d1b3bef465c2d08fadfd91b
ariefgr <gilangsongolas@gmail.com>
2019-12-23 11:16:48 +0700

    uploadfirst

---
+++ modul/tentang.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" >
+
+<head>
+  <meta charset="UTF-8">
+  <title>Tentang - SisPak AI</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
+
+  <link rel='stylesheet prefetch' href='aset/tentang/style.css'>
+  <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css'>
+  <link rel="stylesheet" href="aset/login/css/style.css">
+
+
+</head>
+<style>
+@media only screen and (max-width: 400px) {
+  img {
+    width: 100%;
+  }
+}
+</style>
+<body>
+
+  <div class="ma3">
+    <article class="tc w-75 center pt5 pb2 ph3 mw6-ns ba bw1 b--light-gray" style="background: #fff;">
+      <header class="mb4">
+        <img class="br-100" src="gambar/admin/favicon.png" alt="Profile headshot" />
+        <h1 class="f3 lh-title mv2 dark-gray">Sispak AI </h1>
+        <p class="f6 silver mt2 mb0">
+          <a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Pengembang Aplikasi"><i class="fa fa-user" aria-hidden="true"></i> Arief Gilang Ramadhan</button></a>
+          <a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Pakar Aplikasi"><i class="fa fa-user-md" aria-hidden="true"></i> drh. Agus Toto T</button></a>
+          <a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Dosen Pembimbing 1"><i class="fa fa-user-plus" aria-hidden="true"></i> Teguh S, S.Kom, M.Cs</button></a>
+          <a class="link dim silver"><button style="margin-bottom: 10px;" type="button" class="btn btn-secondary" data-toggle="tooltip" data-placement="bottom" title="Dosen Pembimbing 2"><i class="fa fa-user-plus" aria-hidden="true"></i> Iwan Ady P, S.Kom, M.Kom</button></a></p>
+          <br>
+          <h2 class="f5 silver mt2 mb1">Sistem Pakar, Diagnosa Penyakit AI Pada Bebek</h2>
+          <h2 class="f5 silver mt2 mb1">Copyright © 2019, <a class="link dim silver">STMIK Sinar Nusantara</a></h2>
+          <br>
+
+        </header>
+        <p align="justify" class="f6 tl lh-copy silver" style="margin: 20px;">Sistem pakar yang mampu mendiagnosa penyakit AI pada Bebek berdasarkan pengetahuan yang diberikan langsung dari pakar/ahlinya. Penelitian ini menggunakan metode perhitungan Certainty Factor (CF) dalam menghitung tingkat kepakaran. Data penelitian ini terdiri dari data gejala dan data penyakit Bebek, serta data aturan. Sistem pakar pada organisasi ditujukan  untuk penambahan  value, peningkatan produktivitas serta area manajerial yang dapat mengambil kesimpulan dengan cepat.</p>
+      </article>
+    </div>
+  </body>
+
+  </html>
